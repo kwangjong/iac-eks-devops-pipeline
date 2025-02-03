9f25f2c5c7fbe43c8ddce8e92e244bcf4fbcfbe8
kwangjong <choikj0903@gmail.com>
2025-02-03 21:16:53 +0900

    add provider.tf

---
+++ tf/mgmt/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "iac-eks-devops-pipeline"
+    key            = "mgmt/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
